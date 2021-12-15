798ba437a8bc4470187450b9d3c3d6c6404c37e5
an.nguyen <an.nguyen@9thwonder.com>
2021-12-15 14:16:00 +0700

    swagger document

--- src/app/team/dto/update-team.dto.ts
+++ src/app/team/dto/update-team.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types'
+import { PartialType } from '@nestjs/swagger'
 import { CreateTeamDto } from './create-team.dto'
 
 export class UpdateTeamDto extends PartialType(CreateTeamDto) {}
